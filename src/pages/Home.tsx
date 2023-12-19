946aa46bd5f38fee84723693f3f2132f543a974b
aburahan8721 <aburaihan8721@gmail.com>
2023-12-19 15:29:14 +0600

    done

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,12 +8,9 @@ export default function Home() {
     const fetchUsers = async () => {
       try {
         const response = await fetch("http://localhost:5000/api/users");
-        if (response.ok) {
-          const data = await response.json();
-          setUsers(data.data.users);
-        } else {
-          throw new Error("Data not found with this resources!");
-        }
+        if (!response.ok) throw new Error("Data not found with this resources!");
+        const data = await response.json();
+        setUsers(data.data.users);
       } catch (error) {
         // create error state and set the error
         console.error(error);
